a332f50185c1aa880a2ae2455c4cb8fc91132304
Nicholas Rooker <nrooker@siue.edu>
2023-02-05 23:40:34 -0600

    Add project files.

---
+++ menu/js/main.js
@@ -0,0 +1,6 @@
+﻿const trigger = document.querySelector('.trigger');
+const nav = document.querySelector('.full-screen-nav');
+const backdrop = document.querySelector('.backdrop');
+
+trigger.addEventListener('click', () => nav.classList.add('open-nav'));
+backdrop.addEventListener('click', () => nav.classList.remove('open-nav'));
