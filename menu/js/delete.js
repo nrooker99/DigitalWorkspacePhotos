4d54339cb98dd123511d52e18ff275af2f54b017
Nicholas Rooker <nrooker@siue.edu>
2023-02-19 22:49:57 -0600

    Added delete and add photo pages

---
+++ menu/js/delete.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    var isSelectShowed = false;
+    $('#select').click(function () {
+        if (isSelectShowed) {
+            $('#sel-option').hide();
+        } else {
+            $('#sel-option').show();
+        }
+        isSelectShowed = !isSelectShowed;
+    });
+
+    $('#sel-option a').click(function (e) {
+        $('#select').text($(this).text());
+        $('#sel-option').hide();
+        $(this).addClass('current');
+        e.preventDefault();
+        isSelectShowed = false;
+    })
+
+    $("ul").on("click", "button", function (e) {
+        e.preventDefault();
+        $(this).parent().remove();
+    });
+})
